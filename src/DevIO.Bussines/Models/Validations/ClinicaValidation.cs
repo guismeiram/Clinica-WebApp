242dbc87c076b4da397932aef50d1d05a21489f8
Guilherme Mendes <boralagui@outlook.com>
2022-10-08 20:18:37 -0300

    Atualizando projeto, Repository.

--- src/DevIO.Bussines/Models/Validations/ClinicaValidation.cs
+++ src/DevIO.Bussines/Models/Validations/ClinicaValidation.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DevIO.Bussines.Models.Validations.Documentos;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace DevIO.Bussines.Models.Validations
 {
-    public class ClinicaValidation
+    public class ClinicaValidation : AbstractValidator<Clinica>
     {
+        public ClinicaValidation()
+        {
+            
+        }
+        /* public string Name { get; set; }
+
+        // relacionamentos
+        public virtual List<Consulta> Consultas { get; set; }
+
+        public virtual List<TelefoneClinica> TelefoneClinicas { get; set; }*/
     }
 }
