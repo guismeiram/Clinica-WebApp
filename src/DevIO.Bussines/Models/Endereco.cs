d6949897e5d187915ae700e77397a9b53931a4b5
Guilherme Mendes <boralagui@outlook.com>
2022-10-08 17:01:45 -0300

    Adicionando a classe Telefone e mapeando na classe mapping.

--- src/DevIO.Bussines/Models/Endereco.cs
+++ src/DevIO.Bussines/Models/Endereco.cs
@@ -8,9 +8,15 @@ namespace DevIO.Bussines.Models
 {
     public class Endereco : Entity
     {
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Pais { get; set; }
         public string MedicoId { get; set; }
 
         // relacionamentos
         public virtual Medico Medico { get; set; }
+
     }
 }
