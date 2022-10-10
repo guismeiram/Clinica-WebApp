701a03491833c2aa25c2036afac1dde28d646603
Guilherme Mendes <boralagui@outlook.com>
2022-10-10 18:50:00 -0300

    commitando projeto, camada DevIO.App

--- src/DevIO.App/Program.cs
+++ src/DevIO.App/Program.cs
@@ -1,3 +1,4 @@
+using DevIO.App.Configurations;
 using DevIO.Data.Context;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,13 +9,49 @@ builder.Configuration
     .AddJsonFile("appsettings.json", true, true)
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
     .AddEnvironmentVariables();
+// ConfigureServices
+builder.Services.AddIdentityConfiguration(builder.Configuration);
 
 builder.Services.AddDbContext<ClinicaDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddMvcConfiguration();
+
+builder.Services.ResolveDependencies();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/erro/500");
+    app.UseStatusCodePagesWithRedirects("/erro/{0}");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseGlobalizationConfig();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
+
 
