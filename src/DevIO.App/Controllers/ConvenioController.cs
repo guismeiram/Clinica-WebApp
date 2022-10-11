dbd2ecd36abe0b415c886909689475e18fe66178
Guilherme Mendes <boralagui@outlook.com>
2022-10-10 22:45:42 -0300

    primeiro commit nas controller

---
+++ src/DevIO.App/Controllers/ConvenioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.App.Controllers
+{
+    public class ConvenioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
